4c8dbd92148874344cf6f4f1a2d37b7f8af38a0f
selimbalcik <126884756+selimbalcik@users.noreply.github.com>
2025-05-19 11:40:42 +0300

    tokenizer için stanza kütüphanesinin kullanılması sağlandı.

--- puanBLEU.py
+++ puanBLEU.py
@@ -1,16 +1,28 @@
 import nltk
 from nltk.translate.bleu_score import sentence_bleu
 #nltk.download('punkt')
-import tokenizer
+import stanza
 
+# Türkçe modelini indir ve yükle
+stanza.download("tr")
+nlp = stanza.Pipeline(lang="tr", processors="tokenize")
+
+def tokenize_text(text):
+    doc = nlp(text)
+    tokens = [word.text for sent in doc.sentences for word in sent.words]
+    return tokens
+
+# Örnek kullanım
+# sentence = "Merhaba! Stanza ile cümleleri tokenlere ayırıyorum."
+# tokenized_words = tokenize_text(sentence)
+# print(tokenized_words)
 def bul_puanBLEU(referans_metni,test_metni):
 
   kck_referans_metni = referans_metni.lower()
   kck_test_metni = test_metni.lower()
 
-  tokenizer_instance = tokenizer.Tokenizer()
-  arr_TokenRef=tokenizer_instance.tokenize(kck_referans_metni)
-  arr_TokenTxt=tokenizer_instance.tokenize(kck_test_metni)
+  arr_TokenRef=tokenize_text(kck_referans_metni)
+  arr_TokenTxt=tokenize_text(kck_test_metni)
   # print (arr_TokenRef, arr_TokenTxt)
 
   arr_arrTokenRef = [arr_TokenRef]
